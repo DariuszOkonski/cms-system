40b9845e8f66a67d053078a6a1d4774658626ea2
Dariusz Okonski <darek200180@gmail.com>
2020-09-29 13:32:02 +0100

    divide file index.php into smaller files

--- includes/db.php
+++ includes/db.php
@@ -12,8 +12,8 @@
 
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    if($connection) {
-        echo "We are connected";
+    if(!$connection) {
+        echo "We are not connected: ";
     }
 
 ?>
