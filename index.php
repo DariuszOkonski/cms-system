83438f7e698c1835f3f806abbcdb4825554abba0
Dariusz Okonski <darek200180@gmail.com>
2020-10-01 12:02:48 +0100

    add display posts funcionality work at file index.php main

--- index.php
+++ index.php
@@ -11,14 +11,48 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
 
+                <?php 
+
+                    $query = "SELECT * FROM posts";
+                    $select_all_posts = mysqli_query($connection, $query);
+
+
+                    while ($row = mysqli_fetch_assoc($select_all_posts)):
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_content = $row['post_content'];
+                ?>   
+                    <h2>
+                        <a href="#"><?php echo $post_title; ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on: <?php echo $post_date ?></p>
+                    <hr>
+                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>               
+                <?php endwhile; ?>
+
+
+
+
+
+
+
+
                 <!-- First Blog Post -->
-                <h2>
+                <!-- <h2>
                     <a href="#">Blog Post Title</a>
                 </h2>
                 <p class="lead">
@@ -31,7 +65,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
+                <hr> -->
 
                 <!-- Pager -->
                 <!-- <ul class="pager">
