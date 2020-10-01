19cad22548f773286b565dfc0f1676062dfc9f2e
Dariusz Okonski <darek200180@gmail.com>
2020-10-01 12:25:07 +0100

    add images to db

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
                     <small>Secondary Text</small>
                 </h1>
 
+            <!-- First Blog Post -->
                 <?php 
 
                     $query = "SELECT * FROM posts";
@@ -37,36 +38,13 @@
                     </p>
                     <p><span class="glyphicon glyphicon-time"></span> Posted on: <?php echo $post_date ?></p>
                     <hr>
-                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                    <img class="img-responsive" src="./images/<?php echo $post_image; ?>" alt="">
                     <hr>
                     <p><?php echo $post_content ?></p>
                     <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                     <hr>               
-                <?php endwhile; ?>
-
-
-
-
-
-
-
-
-                <!-- First Blog Post -->
-                <!-- <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr> -->
+                <?php endwhile; ?>               
 
                 <!-- Pager -->
                 <!-- <ul class="pager">
