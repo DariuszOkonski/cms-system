79091f0eb0065394845b64a3b2656bb435beb4e2
Dariusz Okonski <darek200180@gmail.com>
2020-10-07 12:38:50 +0100

    add table to categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -22,7 +22,6 @@
                         
                         <div class="col-xs-6">
                             <form action="" method="">
-
                                 <div class="form-group">
                                     <label for="cat_title">Add Category</label>
                                     <input class="form-control" type="text" name="cat_title" id="cat_title">
@@ -30,9 +29,34 @@
                                 <div class="form-group">
                                     <input class="btn btn-primary" type="submit" name="submit" value="Add Category" >
                                 </div>
-
                             </form>
-                        </div>                       
+                        </div>   
+
+                        <div class="col-xs-6">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>Baseball Category</td>
+                                        <td>Basketball Category</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Baseball Category</td>
+                                        <td>Basketball Category</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Baseball Category</td>
+                                        <td>Basketball Category</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>                    
 
 
                     </div>
