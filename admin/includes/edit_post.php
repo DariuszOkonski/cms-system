2591c47dd3da89c97c269af097d38bb4b35df383
Dariusz Okonski <darek200180@gmail.com>
2020-10-14 13:46:47 +0100

    add edit_post query and display all data at form

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,7 +1,22 @@
 <?php 
 
-    if(isset($_GET['source'])) {
-        print_r($_GET);
+    if(isset($_GET['p_id'])) {
+        $the_post_id = $_GET['p_id'];
+
+        $query = "SELECT * FROM posts WHERE post_id = {$the_post_id}";  
+        $select_posts_by_id = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_assoc($select_posts_by_id)) {
+            $post_author = $row['post_author'];
+            $post_title = $row['post_title'];
+            $post_category_id = $row['post_category_id'];
+            $post_status = $row['post_status'];
+            $post_image = $row['post_image'];
+            $post_tags = $row['post_tags'];
+            $post_comment_count = $row['post_comment_count'];
+            $post_date = $row['post_date'];
+            $post_content = $row['post_content'];
+        }
     }
 
 ?>
@@ -10,37 +25,41 @@
     <h1>Edit Post</h1>
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title" required >
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title" required >
     </div>
 
     <div class="form-group">
         <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id" required >
+        <input value="<?php echo $post_category_id; ?>" type="text" class="form-control" name="post_category_id" required >
     </div>
 
     <div class="form-group">
         <label for="author">Post Author</label>
-        <input type="text" class="form-control" name="author" required >
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author" required >
     </div>
 
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status" required >
+        <input value="<?php echo $post_status ?>" type="text" class="form-control" name="post_status" required >
     </div>
 
+    <!-- TODO set image value -->
     <div class="form-group">
         <label for="post_image">Post Image</label>
+        <label for="post_image"> => <?php echo $post_image ?></label>
         <input type="file" name="image" required >
     </div>
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags" required > 
+        <input value="<?php echo $post_tags; ?>" type="text" class="form-control" name="post_tags" required > 
     </div>
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea required name="post_content" id="" cols="30" rows="10" class="form-control"></textarea>
+        <textarea required name="post_content" id="" cols="30" rows="10" class="form-control">
+            <?php echo $post_content; ?>
+        </textarea>
     </div>
 
     <div class="form-group">
