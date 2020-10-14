36e21ddf505171d47615522079c92e90f54d921c
Dariusz Okonski <darek200180@gmail.com>
2020-10-14 12:56:34 +0100

    add funcionality to remove single post

--- admin/functions.php
+++ admin/functions.php
@@ -59,37 +59,4 @@ function deleteCategories() {
     }
 }
 
-function findAllPosts() {
-    global $connection;
-    
-    $query = "SELECT * FROM posts";
-    $select_posts = mysqli_query($connection, $query);
-
-    while($row = mysqli_fetch_assoc($select_posts)) {
-        $post_id = $row['post_id'];
-        $post_author = $row['post_author'];
-        $post_title = $row['post_title'];
-        $post_category_id = $row['post_category_id'];
-        $post_status = $row['post_status'];
-        $post_image = $row['post_image'];
-        $post_tags = $row['post_tags'];
-        $post_comment_count = $row['post_comment_count'];
-        $post_date = $row['post_date'];
-
-        echo "
-            <tr>
-                <td>{$post_id}</td>
-                <td>{$post_author}</td>
-                <td>{$post_title}</td>
-                <td>{$post_category_id}</td>
-                <td>{$post_status}</td>
-                <td><img class='img-responsive' width='100' src='../images/{$post_image}' alt='images' /></td>
-                <td>{$post_tags}</td>
-                <td>{$post_comment_count}</td>
-                <td>{$post_date}</td>
-            </tr>
-        ";
-    }
-}
-
 ?>
--- admin/posts.php
+++ admin/posts.php
@@ -36,6 +36,7 @@
                                     include("includes/view_all_posts.php");
                                     break;
                             }
+
                         ?>
 
                     </div>
