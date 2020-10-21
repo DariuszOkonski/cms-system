e5133392e50e27e3f23c8338130284aec78cfca8
Dariusz Okonski <darek200180@gmail.com>
2020-10-21 12:58:37 +0100

    display single post

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
                     $select_all_posts = mysqli_query($connection, $query);
 
                     while ($row = mysqli_fetch_assoc($select_all_posts)):
+                        $post_id = $row['post_id'];
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
                         $post_image = $row['post_image'];
@@ -30,7 +31,7 @@
                         $post_content = $row['post_content'];
                 ?>   
                     <h2>
-                        <a href="#"><?php echo $post_title; ?></a>
+                        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                     </h2>
                     <p class="lead">
                         by <a href="index.php"><?php echo $post_author ?></a>
@@ -40,7 +41,7 @@
                     <img class="img-responsive" src="./images/<?php echo $post_image; ?>" alt="">
                     <hr>
                     <p><?php echo $post_content ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                     <hr>               
                 <?php endwhile; ?>               
